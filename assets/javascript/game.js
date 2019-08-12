93dcdaefdb5174d6040d1c2de7d8c8e9a7f3c838
BrittGall <crlnfllr@gmail.com>
2019-08-11 23:56:34 -0500

    all get elements

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+// list out varibles
+
+// // Number we trying to match MUST BE BETWEEN 19-120 and randomly selected
+targetNumberElement = document.getElementById("targetNumber");
+
+// // The sum of the crystal click scores, will change
+collectedCrystalsElements = document.getElementById("collectedCrystals");
+
+// // * Each crystal should have a random hidden value between 1 - 12.
+// blue crystal
+blueElement = document.getElementById("blue");
+
+// green crystal
+greenElement = document.getElementById("green");
+
+// yellow crystal
+yellowElement = document.getElementById("yellow");
+
+// pink crystal
+pinkElement = document.getElementById("pink");
+
+// total wins
+winsElement = document.getElementById("wins");
+
+// total loses
+losesElement = document.getElementById("loses");
