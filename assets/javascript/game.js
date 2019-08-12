5727e30511825c554ddece63cde092ff288fdd40
BrittGall <crlnfllr@gmail.com>
2019-08-12 00:33:55 -0500

    psuedo coding

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,6 @@ targetNumberElement = $("targetNumber");
 // // The sum of the crystal click scores, will change
 collectedCrystalsElements = $("collectedCrystals");
 
-// // * Each crystal should have a random hidden value between 1 - 12.
 // blue crystal
 blueElement = $("blue");
 
@@ -24,3 +23,11 @@ winsElement = $("wins");
 
 // total loses
 losesElement = $("loses");
+
+// game starts on page load
+// wins, loses, and collected crystals are 0
+// target number is randomly selected (a number between 19-120)
+
+// each of the 4 crystals (blue, green, yellow, and pink ) are assigned a random number between 1-12, the number is hidden from the player
+
+// on click, the number assigned to the crystal is added to collectedCrystals
